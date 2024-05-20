b47e602617368d183d765a3ca52776f1f39735cd
Guilherme Silva Piantamar <guilhermepiantamar@gmail.com>
2024-05-20 10:58:49 -0300

    Variáveis e constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
     public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Variáveis em JAVA
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+        System.out.println("Variáveis");
+        System.out.println("byte: " + idade + "\nshort: " + ano + "\nint: " + cep + "\nlong: " + cpf + "\npi: " + pi + "\nsalario: " + salario);
+
+        System.out.println("Constantes");
+        //CONSTANTES em JAVA
+        final double valorPi = 3.14;
+        System.out.println(valorPi);
     }
 }
