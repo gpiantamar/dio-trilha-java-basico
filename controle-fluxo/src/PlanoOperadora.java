1a267cb0c3ac6aa4301187f23bfb41ff424a363b
Guilherme Silva Piantamar <guilhermepiantamar@gmail.com>
2024-05-22 14:29:02 -0300

    condicionais

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,30 @@
+
+public class PlanoOperadora {
+	public static void main(String[] args) {
+		String plano = "B";
+		/*Modo condicional convencional
+		if(plano == "B") {
+			System.out.println("100 minutos de ligação");
+		}else if(plano == "M") {
+			System.out.println("100 minutos de ligação");
+			System.out.println("Whats e Intagram grátis");	
+		}else if(plano == "T") {
+			System.out.println("100 minutos de ligação");
+			System.out.println("Whats e Intagram grátis");	
+			System.out.println("5Gb Youtube");{
+		*/
+		//Modo condicional switch case. (Foi usado de modo complementar do mais Complto para o mais simples)
+		switch (plano) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+			}
+			case "M": {
+				System.out.println("Whats e Intagram grátis");
+			}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+		}
+	}
+}
+
