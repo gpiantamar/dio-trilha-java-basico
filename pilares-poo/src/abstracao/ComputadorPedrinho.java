d90da9794686636a1f31ff6b0471f4a800f164e5
Guilherme Silva Piantamar <guilhermepiantamar@gmail.com>
2024-05-27 13:49:09 -0300

    abstração

---
+++ pilares-poo/src/abstracao/ComputadorPedrinho.java
@@ -0,0 +1,19 @@
+package abstracao;
+public class ComputadorPedrinho {
+	public static void main(String[] args) throws Exception{
+		System.out.println("Facebook");
+		FacebookMessenger fcb = new FacebookMessenger();
+		fcb.enviarMensagem();
+		fcb.receberMensagem();
+
+		System.out.println("MSN");
+		MSNMessenger msn = new MSNMessenger();
+		msn.enviarMensagem();
+		msn.receberMensagem();
+
+		System.out.println("Telegram");
+		Telegram tlg = new Telegram();
+		tlg.enviarMensagem();
+		tlg.receberMensagem();
+	}
+}
